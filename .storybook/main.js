2c0b82c63d1f3a8282fd9c46636e73d47ad21521
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2023-05-11 17:03:08 +0200

    added chromatic for visual regression testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,20 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
+    '@storybook/react',
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
-  staticDirs: ["../public"],
+  staticDirs: ['../public'],
 };
 export default config;
